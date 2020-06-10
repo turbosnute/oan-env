15a86244ee2075f62eca0410e0bad894e7764a76
Øyvind Nilsen <oyvind.nilsen@nhn.no>
2020-06-10 13:33:10 +0200

    psprofile is added to existing profile instead of beeing dotsourced

--- load-oan-env.ps1
+++ load-oan-env.ps1
@@ -1,7 +1,15 @@
 #
 # File urls
 #
-$psOanProfileUri = ""
+$psOanProfileUri = "https://raw.githubusercontent.com/turbosnute/oan-env/master/psprofile/oan-profile.ps1"
+
+
+#
+# Downloading
+#
+$tempOanProfile = New-TemporaryFile
+Invoke-WebRequest -uri $psOanProfileUri -OutFile $tempOanProfile.FullName
+$oanProfileBlock = Get-Content $tempOanProfile.FullName
 
 $fg = "Yellow"
 $success = "Green"
@@ -18,27 +26,40 @@ $psProfileDir = Split-Path $PROFILE
 $psOanProfilePath = Join-Path -Path $psProfileDir -ChildPath $psOanProfileFile
 [bool]$dotsourcingFail = $false
 
+$startIndicator = "#<OAN-ENV START>"
+$stopIndicator = "#<OAN-ENV STOP>"
+
 Write-Host -ForegroundColor $gf "Current Profile: $PROFILE"
-Write-Host -ForegroundColor $fg "- Checking if dotsourcing is ok."
-
-try {
-. $PROFILE
-} catch [System.Management.Automation.RuntimeException] {
-  Write-Host -ForegroundColor $fail "  [DotSourcing not ok]"
-}
-
-if ($dotsourcingFail) {
-    Write-Host -ForegroundColor $fg "  Using trusted directory istead ($dotsourcingFail)"
-    $psOanProfilePath = Join-Path -Path $trustedDirectory -ChildPath $psOanProfileFile
-} else {
-    Write-Host -ForegroundColor $success "  [DotSourcing ok]"
-}
 Write-Host -ForegroundColor $fg "- Looking for oan-env..."
 
-if (Test-Path $psOanProfilePath) {
-    Write-Host -ForegroundColor $success "  [$psOanProfileFile Exists. Will ble updated]"
+[bool]$psEnvEagleHasLanded = $false
+[bool]$startIndicatorReached = $false
+[bool]$stopIndicatorReached = $false
+$currentProfileText = Get-Content $PROFILE
+$newProfileText = @()
+
+$currentProfileText | % {
+    if ($_ -eq $startIndicator) {
+        $newProfileText += $_
+        $startIndicatorReached = $true
+        write-host -ForegroundColor cyan $_
+    } elseif ($_ -eq $stopIndicator) {
+        $newProfileText += $_
+        $stopIndicatorReached = $true
+        $startIndicatorReached = $false
+        write-host -ForegroundColor cyan $_
+    } elseif ($startIndicatorReached -eq $false) {
+        $newProfileText += $_
+        Write-Host $_
+    } else {
+        #Dette bør være inni blokken
+        if(-not $psEnvEagleHasLanded) {
+            Write-Host -ForegroundColor Green "  [Found oan-env...]"
+            $newProfileText += $oanProfileBlock
+            Write-Host -ForegroundColor Green "  [Updated]"
+            $psEnvEagleHasLanded = $true
+        }
+    }
 }
-Write-Host -ForegroundColor $fg "  Downloading $($psOanProfileFile)..."
-Invoke-WebRequest -Uri -OutFile $psOanProfilePath
 
 Write-Host -ForegroundColor $fg "- Checking if $psOanProfile included in PROFILE"
